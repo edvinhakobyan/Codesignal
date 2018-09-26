1a3d542e2e658b1ae12f3800e7aa1f7aa4471086
Edvin <edvin-hakobyan@mail.ru>
2018-09-26 23:41:26 +0400

    add checkPalindrome

---
+++ checkPalindrome/checkPalindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = checkPalindrome("aaaabbybbaaaa");
+        }
+
+        public static bool checkPalindrome(string inputString)
+        {
+            return inputString == new string(inputString.Reverse().ToArray());
+        }
+    }
+}
