8eee57d51e496b642a3b4d255921f5f9d1be7d0f
Edvin <edvin-hakobyan@mail.ru>
2018-09-26 23:14:16 +0400

    add

---
+++ add/add/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int add(int param1, int param2)
+        {
+         return param1 + param2;
+        }
+    }
+}
