fd981de9db60a58cfb032ccbf2cf1f22a0d2885b
Edvin <edvin-hakobyan@mail.ru>
2018-09-26 23:25:42 +0400

    add centuryFromYear

---
+++ centuryFromYear/centuryFromYear/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace centuryFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            centuryFromYear(1988);
+        }
+
+        public static int centuryFromYear(int year)
+        {
+            return year % 100 == 0 ? year / 100 : year / 100 + 1;
+        }
+    }
+}
