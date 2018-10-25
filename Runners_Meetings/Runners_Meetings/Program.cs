c814bd74939f4fe6c7903f98c6c63cdef2834303
edvinhakobyan <edvin1984edvin@gmail.com>
2018-10-25 11:15:54 +0400

    add christmasTree

---
+++ Runners_Meetings/Runners_Meetings/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runners_Meetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            runnersMeetings(new int[] { 1, 4, 2 },new int[] { 27, 18, 24 });
+        }
+
+
+
+        static int runnersMeetings(int[] stPoint, int[] speed)
+        {
+            int count = 0;
+            for (double t = 0; t < 1000; t+=0.001)
+            {
+                double[] currentPosition = new double[stPoint.Length];
+                for (int i = 0; i < stPoint.Length; i++)
+                    currentPosition[i] += Math.Round((stPoint[i] + t * speed[i]), 2);
+
+                for (int j = 0; j < currentPosition.Length; j++)
+                {
+                    int mm = currentPosition.Count(a => a == currentPosition[j]);
+                    if (mm > count) count = mm;
+                }
+            }
+            return count;
+        }
+
+            static int runnersMeetings1(int[] stP, int[] s)
+        {
+            int mult = 1;
+            for (int i = 0; i < s.Length; mult *= s[i], i++) ;
+
+            int min = stP.Min();
+            for (int i = 0; i < stP.Length; i++)
+            {
+                if(min>=0)
+                  stP[i] -= Math.Abs(min);
+                else
+                    stP[i] += Math.Abs(min);
+            }
+
+            int[] arrBool = new int[10*mult];
+
+            while (true)
+            {
+                for (int i = 0; i < stP.Length; i++)
+                {
+                    for (int j = stP[i]; j < stP[i] + s[i]; j++)
+                        arrBool[j]++;
+                    stP[i] = stP[i] + s[i];
+                }
+            }
+
+
+            for (int i = 0; i < arrBool.Length; i++)
+            {
+                if (arrBool[i] > 1)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            
+
+
+
+
+
+
+
+                return 1;
+        }
+    }
+}
---
+++ christmasTree/christmasTree/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace christmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string[] christmasTree(int levelNum, int levelHeight)
+        {
+            int ind = 0, L = 3 + levelNum * levelHeight + levelNum;
+            string[] ret = new string[L];
+
+            ret[ind++] = "*".PadLeft(levelNum + levelHeight + 1);
+            ret[ind++] = "*".PadLeft(levelNum + levelHeight + 1);
+            ret[ind++] = "***".PadLeft(levelNum + levelHeight + 2);
+
+            for (int i = 0, j = 0; i < levelNum; i++, j += 2)
+                for (int k = 0, l = 5 + j; k < levelHeight; k++, l += 2)
+                    ret[ind++] = new string('*', l).PadLeft(l + levelNum + levelHeight - (k + 2) - i);
+
+            int foot_W = levelHeight % 2 == 0 ? levelHeight + 1 : levelHeight;
+            int max_W = 2 * (levelNum + levelHeight) + 1;
+
+            for (int i = 0; i < levelNum; i++)
+                ret[ind++] = new string('*', foot_W).PadLeft((foot_W + max_W) / 2);
+
+
+            for (int i = 0; i < ret.Length; i++)
+            {
+                Console.WriteLine(ret[i]);
+            }
+
+            return ret;
+        }
+        /*
+         * levelNum: 4
+         * levelHeight: 8
+         * 
+         * 
+        ["            *", 
+         "            *", 
+         "           ***", 
+         "          *****", 
+         "         *******", 
+         "        *********", 
+         "       ***********", 
+         "      *************", 
+         "     ***************", 
+         "    *****************", 
+         "   *******************", 
+         "         *******", 
+         "        *********", 
+         "       ***********", 
+         "      *************", 
+         "     ***************", 
+         "    *****************", 
+         "   *******************", 
+         "  *********************", 
+         "        *********", 
+         "       ***********", 
+         "      *************", 
+         "     ***************", 
+         "    *****************", 
+         "   *******************", 
+         "  *********************", 
+         " ***********************", 
+         "       ***********", 
+         "      *************", 
+         "     ***************", 
+         "    *****************", 
+         "   *******************", 
+         "  *********************", 
+         " ***********************", 
+         "*************************", 
+         "        *********", 
+         "        *********", 
+         "        *********", 
+         "        *********"]
+         */
+    }
+}
